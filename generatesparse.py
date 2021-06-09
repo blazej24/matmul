9b3a54bd2d18bc9cbf3b1894cbd8a474ac356bfb
Your Name <you@example.com>
2021-06-09 17:39:33 +0200

    fix generate sparse

--- generatesparse.py
+++ generatesparse.py
@@ -5,6 +5,11 @@ N = int(sys.argv[1])
 NNZ_per_row = int(sys.argv[2])
 
 print(str(N), str(N), str(N*NNZ_per_row), str(NNZ_per_row))
+
+for _ in range(N * NNZ_per_row):
+    print(round(random.uniform(1.2, 5931.3), 5), end=' ')
+print()
+
 idx = 0
 for i in range(N):
     print(idx, end=' ')
